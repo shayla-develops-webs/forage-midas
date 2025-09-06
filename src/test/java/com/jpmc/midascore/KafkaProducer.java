b4fba81743eacde5aa40cca3e260f01907c3a518
shayla-develops-webs <shaylamrodgers@gmail.com>
2025-09-05 19:55:25 -0500

    kafka listener added

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -1,5 +1,6 @@
 package com.jpmc.midascore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,21 +10,34 @@ import org.springframework.stereotype.Component;
 public class KafkaProducer {
 
     private final String topic;
-    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper(); // for JSON conversion
 
     public KafkaProducer(@Value("${general.kafka-topic}") String topic,
-                         KafkaTemplate<String, Transaction> kafkaTemplate) {
+                         KafkaTemplate<String, String> kafkaTemplate) {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void send(String transactionLine) {
-        String[] transactionData = transactionLine.split(", ");
-        Transaction transaction = new Transaction(
-                Long.parseLong(transactionData[0]),
-                Long.parseLong(transactionData[1]),
-                Float.parseFloat(transactionData[2])
-        );
-        kafkaTemplate.send(topic, transaction);
+        try {
+            // Parse input string into Transaction object
+            String[] transactionData = transactionLine.split(", ");
+            Transaction transaction = new Transaction(
+                    Long.parseLong(transactionData[0]),
+                    Long.parseLong(transactionData[1]),
+                    Float.parseFloat(transactionData[2])
+            );
+
+            // Convert Transaction to JSON
+            String transactionJson = objectMapper.writeValueAsString(transaction);
+
+            // Send JSON string to Kafka
+            kafkaTemplate.send(topic, transactionJson);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send Transaction to Kafka", e);
+        }
     }
 }
+
--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -1,20 +1,12 @@
 package com.jpmc.midascore;
 
-import com.jpmc.midascore.foundation.Transaction;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
-
-import java.util.Map;
 
 @SpringBootTest
 @DirtiesContext
@@ -23,49 +15,27 @@ class TaskTwoTests {
     static final Logger logger = LoggerFactory.getLogger(TaskTwoTests.class);
 
     @Autowired
-    private FileLoader fileLoader;
-
-    @Autowired
     private KafkaProducer kafkaProducer;
 
     @Autowired
-    private KafkaTemplate<String, Transaction> originalKafkaTemplate;
-
-    @Value("${spring.embedded.kafka.brokers}")
-    private String embeddedBrokers;
-
-    @BeforeEach
-    void setup() {
-        // Override KafkaTemplate to point to embedded Kafka broker
-        Map<String, Object> producerProps = KafkaTestUtils.producerProps(embeddedBrokers);
-        KafkaTemplate<String, Transaction> testTemplate =
-                new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(producerProps));
-
-        // Replace the KafkaProducer's template with the test template
-        kafkaProducer = new KafkaProducer("transactions", testTemplate);
-    }
+    private FileLoader fileLoader;
 
     @Test
     void task_two_verifier() throws InterruptedException {
-        // Load test transactions
         String[] transactionLines = fileLoader.loadStrings("/test_data/poiuytrewq.uiop");
         for (String transactionLine : transactionLines) {
             kafkaProducer.send(transactionLine);
         }
-
-        // Wait briefly for transactions to be consumed
         Thread.sleep(2000);
-
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
         logger.info("----------------------------------------------------------");
-        logger.info("Use your debugger to watch for incoming transactions");
-        logger.info("Kill this test once you have the first four amounts");
-
-        // Keep test running so you can see transactions in the listener
+        logger.info("use your debugger to watch for incoming transactions");
+        logger.info("kill this test once you find the answer");
         while (true) {
             Thread.sleep(20000);
             logger.info("...");
         }
     }
+
 }
